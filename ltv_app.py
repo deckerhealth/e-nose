734b09855e7c1ef1fb1c9afc5095da67fbb57e03
deckerhealth <anton@deckerhealth.org>
2025-07-07 18:38:41 +0100

    Update ltv_app.py

--- ltv_app.py
+++ ltv_app.py
@@ -3,20 +3,35 @@ import matplotlib.pyplot as plt
 
 # Function to calculate LTV
 def calculate_ltv(upfront_price, monthly_fee, annual_fee_growth, churn_rate, discount_rate,
-                  annual_addon, addon_growth, years):
+                  annual_addon, addon_growth, p_addon, churn_addon, years):
 
     ltv = upfront_price
-    active_rate = 1.0
+    active_sub_rate = 1.0
+    active_addon_rate = p_addon  # Only p_addon percent of users start with add-ons
 
     cash_flows = []
 
     for t in range(1, years + 1):
+        # Subscription revenue
         annual_sub_fee = monthly_fee * 12 * ((1 + annual_fee_growth) ** (t - 1))
+        active_sub_rate = active_sub_rate * (1 - churn_rate)
+        sub_cash_flow = annual_sub_fee * active_sub_rate
+
+        # Add-on revenue
         annual_addon_revenue = annual_addon * ((1 + addon_growth) ** (t - 1))
-        active_rate = active_rate * (1 - churn_rate)
-        cash_flow = (annual_sub_fee + annual_addon_revenue) * active_rate
+        if t == 1:
+            active_addon_rate = p_addon
+        else:
+            active_addon_rate = active_addon_rate * (1 - churn_addon)
+        addon_cash_flow = annual_addon_revenue * active_addon_rate
+
+        # Total cash flow for the year
+        total_cash_flow = sub_cash_flow + addon_cash_flow
+
+        # Discount factor
         discount_factor = 1 / ((1 + discount_rate) ** t)
-        pv = cash_flow * discount_factor
+        pv = total_cash_flow * discount_factor
+
         ltv += pv
         cash_flows.append(ltv)
 
@@ -32,14 +47,18 @@ monthly_fee = st.sidebar.number_input("Monthly subscription fee ($)", 0, 500, 30
 annual_fee_growth = st.sidebar.slider("Annual subscription fee growth rate (%)", 0, 30, 5) / 100
 churn_rate = st.sidebar.slider("Annual churn rate (%)", 0, 50, 10) / 100
 discount_rate = st.sidebar.slider("Discount rate (%)", 0, 30, 8) / 100
+
 annual_addon = st.sidebar.number_input("Annual add-on sales ($)", 0, 5000, 100)
 addon_growth = st.sidebar.slider("Annual add-on growth rate (%)", 0, 30, 7) / 100
+p_addon = st.sidebar.slider("Probability user buys add-ons (%)", 0, 100, 60) / 100
+churn_addon = st.sidebar.slider("Annual churn rate for add-on buyers (%)", 0, 50, 15) / 100
+
 years = st.sidebar.slider("Projection years", 1, 20, 10)
 
 cash_flows, final_ltv = calculate_ltv(
     upfront_price, monthly_fee, annual_fee_growth,
     churn_rate, discount_rate, annual_addon,
-    addon_growth, years
+    addon_growth, p_addon, churn_addon, years
 )
 
 st.write(f"### Estimated total LTV per customer: **${final_ltv:,.2f}**")
